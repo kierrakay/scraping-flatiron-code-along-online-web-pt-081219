0bb071f26f1c994aaa4dda0aeb40fa1c50d88c2b
Kierra M <k.merriman32@hotmail.com>
2019-10-04 15:14:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,23 +33,22 @@ def make_courses
   end
 end
 end
-Scraper.new.print_courses
 # end 
- # Scraper.new.get_page
+  #Scraper.new.get_page
   
   
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
   
-# end
-
+end
+Scraper.new.print_courses
 
 
